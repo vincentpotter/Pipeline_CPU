b3f5fd18818bbea9b5299e5868fde9eefa933701
VecentPotter <59953422+VecentPotter@users.noreply.github.com>
2020-04-26 21:09:40 +0800

    Create instructions.v

---
+++ Control/instructions.v
@@ -0,0 +1,10 @@
+//codes of instructions
+    parameter R_ = 6'b000000, 
+    Addi_ = 6'b001000,
+    Addiu_ = 6'b001001,
+    Lui_ = 6'b001111,
+    Lw_ = 6'b100011,
+    Sw_ = 6'b101011,
+    Branch_ = 6'b000100,
+    Jump_ = 6'b000010,
+    Ori = 6'b001101;
